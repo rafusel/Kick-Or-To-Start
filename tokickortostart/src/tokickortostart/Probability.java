fa516f902b1377098411b825e56dde005439aa78
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-04-03 21:00:01 -0400

    Added implementation for merge sort.

--- tokickortostart/src/tokickortostart/Probability.java
+++ tokickortostart/src/tokickortostart/Probability.java
@@ -1,12 +1,12 @@
 package tokickortostart;
 
-import java.util.Arrays;
-
 public class Probability {
-	public static double probability(Project [] arr) {
-		sortProj(arr);
+	private static Project[] aux;
+	
+	public static double probability(Project[] arr) {
+		sortProj(arr, arr.length);
 		int length = 100;
-		if (arr.length < 100) {
+		if (arr.length < 100 && arr.length != 0) {
 			length = arr.length;
 		}
 		int sum = 0;
@@ -20,7 +20,32 @@ public class Probability {
 		return (double) sum / length;
 	}
 	
-	private static void sortProj(Project [] arr) {
-		Arrays.sort(arr);
+	private static void sortProj(Project[] arr, int n) {
+		aux = new Project[n];
+		sort(arr, 0, n - 1);
+	}
+	
+	private static void sort(Project[] a, int lo, int hi) {
+		if (hi <= lo)
+			return;
+		int mid = lo + (hi - lo) / 2;
+		sort(a, lo, mid); 
+		sort(a, mid + 1, hi);
+		merge(a, lo, mid, hi);
+	}
+	
+	private static void merge(Project[] a, int lo, int mid, int hi) {
+		int i = lo, j = mid + 1;
+		for (int k = lo; k <= hi; k++)
+			aux[k] = a[k];
+		for (int k = lo; k <= hi; k++)
+			if (i > mid)
+				a[k] = aux[j++];
+			else if (j > hi)
+				a[k] = aux[i++];
+			else if (aux[j].compareTo(aux[i]) < 0)
+				a[k] = aux[j++];
+			else
+				a[k] = aux[i++];
 	}
 }
