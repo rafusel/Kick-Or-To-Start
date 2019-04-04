e422a922fe85e25c6bfa0a5c513adf0f192ce518
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-04-03 22:48:21 -0400

    Reads fields up until category.

--- tokickortostart/src/tokickortostart/IO.java
+++ tokickortostart/src/tokickortostart/IO.java
@@ -1,5 +1,38 @@
 package tokickortostart;
 
-public class IO {
+import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
 
+public class IO {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the begin date of your project: (dd/mm/yyyy)");
+		String beginString = input.next();
+		input = new Scanner(beginString);
+		input.useDelimiter("/");
+		int day = Integer.parseInt(input.next());
+		int month = Integer.parseInt(input.next());
+		int year = Integer.parseInt(input.next());
+		Calendar cal = Calendar.getInstance();
+		cal.set(year,  month, day);
+		Date begin = cal.getTime();
+		
+		System.out.println("Please enter the end date of your project: (dd/mm/yyyy)");
+		input = new Scanner(System.in);
+		String endString = input.next();
+		input = new Scanner(endString);
+		input.useDelimiter("/");
+		day = Integer.parseInt(input.next());
+		month = Integer.parseInt(input.next());
+		year = Integer.parseInt(input.next());
+		cal = Calendar.getInstance();
+		cal.set(year,  month, day);
+		Date end = cal.getTime();
+		
+		double duration = Math.abs((begin.getTime() - end.getTime())/8640000);
+		
+		
+		
+	}
 }
