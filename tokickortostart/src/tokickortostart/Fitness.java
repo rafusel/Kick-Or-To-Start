7b313e6e0d2f93e82508ad77ddfe85beb952de0a
Cameron Dufault <dufaultc@mcmaster.ca>
2019-04-14 18:52:35 -0400

    Commented Fitness.java

--- tokickortostart/src/tokickortostart/Fitness.java
+++ tokickortostart/src/tokickortostart/Fitness.java
@@ -1,12 +1,25 @@
 package tokickortostart;
-
+/**
+ * 
+ * This class is used to determine how successful projects 
+ * are, based off of amount of money received
+ *
+ */
 public class Fitness {
+	/**
+	 * Assigns fitness values to all projects in an inputted array
+	 * @param arr The array of project types to assign the fitness of
+	 */
 	public static void allFitness(Project [] arr) {
 		for (int index = 0; index < arr.length; index++) 
 			fitness(arr[index]);
 		return;
 	}
 	
+	/**
+	 * Assigning a fitness value to an individual project 
+	 * @param project The project to assign the fitness of
+	 */
 	private static void fitness(Project project) {
 		double fitness = 100 * project.getPledge() / project.getGoal();
 		
