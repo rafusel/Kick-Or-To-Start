d1840e7e1201e9d1cc273d8e74f5b4c720681706
Cameron Dufault <dufaultc@mcmaster.ca>
2019-04-14 18:22:39 -0400

    Commented Edge.java

--- tokickortostart/src/tokickortostart/Edge.java
+++ tokickortostart/src/tokickortostart/Edge.java
@@ -1,28 +1,57 @@
 package tokickortostart;
-
-//Class used to implement a more simple edge
-
+/**
+ *  Class used to implement a simple edge
+ *
+ */
 public class Edge {
-	private final int start; //Start vertex of the edge
-	private final int end; //End vertex of the edge
-	private final double weight; //Weight of the edge
+	
+	/**
+	 * Start vertex of the edge
+	 */
+	private final int start; 
+	/**
+	 * End vertex of the edge
+	 */
+	private final int end;
+	/**
+	 * Weight of the edge
+	 */
+	private final double weight; 
 
+	/**
+	 * Edge constructor
+	 * 
+	 * @param start The start vertex of the edge
+	 * @param end The end vertex of the edge
+	 * @param weight The weight of the edge
+	 */
 	public Edge(int start, int end, double weight) {
-		//Initialize state variables based of inputs
 		this.start = start;
 		this.end = end;
 		this.weight = weight;
 	}
 	
-	public int getStart() { //Gets the start vertex of the edge
+	/**
+	 * Gets the start vertex of the edge
+	 * @return the start vertex of the edge
+	 */
+	public int getStart() { 
 		return this.start;
 	}
 	
-	public int getEnd() { //Gets the end vertex of the edge
+	/**
+	 * Gets the end vertex of the edge
+	 * @return the end vertex of the edge
+	 */
+	public int getEnd() { 
 		return this.end;
 	}
 	
-	public double getWeight() { //Gets the weight of the edge
+	/**
+	 * Gets the weight of the edge
+	 * @return the weight of the edge
+	 */
+	public double getWeight() {
 		return this.weight;
 	}
 }
