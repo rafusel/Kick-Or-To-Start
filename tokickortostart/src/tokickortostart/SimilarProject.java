2084549de9c7a05ab55ec53be5e05bd8272f1ef6
dufaultc <44153501+dufaultc@users.noreply.github.com>
2019-04-14 20:54:15 -0400

    Commented SimilarProject.java

--- tokickortostart/src/tokickortostart/SimilarProject.java
+++ tokickortostart/src/tokickortostart/SimilarProject.java
@@ -1,8 +1,20 @@
 package tokickortostart;
 
+/**
+ * This module contains the method for returning the most
+ * similar project to the user's project
+ *
+ */
 public class SimilarProject {
+	/**
+	 * This method returns the first element given an array of projects sorted by how similar they are to the user's project
+	 * 
+	 * @param arr An array of projects sorted by how similar they are to the user's project
+	 * @return The most similar project to the user's
+	 */
 	public static Project MostSimilar(Project [] arr) {
 		//This needs to be given a sorted array
 		return arr[0];
 	}
 }
+
